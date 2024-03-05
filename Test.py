9441eb0682837d5d914881d867519f673718cc60
Администратор <ruyesar@gmail.com>
2024-03-05 13:30:51 +0300

    1

--- Test.py
+++ Test.py
@@ -7,30 +7,34 @@ import joblib
 import Analyse
 
 
-filename = 'model_params_2.pkl'
+filename = 'model_params_3.pkl'
 model = joblib.load(filename)
 
 with h5py.File('GIB-UVA ERP-BCI.hdf5', 'r') as f:
     data = f['features']
     labels = f['erp_labels']
-    d = np.mean(data[10000:20000, 0:128, :], 2)
+    d = data[10000:20000, 0:128, 0:8]
     y = labels[10000:20000]
-x = np.zeros((10000, 64))
+# x = np.zeros((10000, 64))
+size = 10000
 Fs = 128
 N = np.shape(d)[1]
-for i in range(0, 10000):
-    x[i, :] = Analyse.Spectrum(d[i, :], Fs, N)
-y_pred = model.predict(x)
+# for i in range(0, 10000):
+#     x[i, :] = Analyse.Spectrum(d[i, :], Fs, N)
+y_pred_0 = model.predict(d)
+y_pred = np.zeros(size)
+for i in range(0, size):
+    y_pred[i] = int(np.round(max(y_pred_0[i, 0], y_pred_0[i, 1])))
 # Оценка точности модели
-accuracy = model.score(x, y)
+# accuracy = model.score(d, y)
 precision = precision_score(y, y_pred)
 recall = recall_score(y, y_pred)
 mse = mean_squared_error(y, y_pred)
 r2 = roc_auc_score(y, y_pred)
 
-print("Точность модели: {:.2f}%".format(accuracy * 100))
+# print("Точность модели: {:.2f}%".format(accuracy * 100))
 print("Чувствительность модели: {:.2f}%".format(precision * 100))
-print("Специфичность модели: {:.2f}%".format(accuracy * 100))
+# print("Специфичность модели: {:.2f}%".format(accuracy * 100))
 print("MSE:", mse)
 print("R-squared:", r2)
 print(np.sum(np.abs(y - y_pred)))
--- Train.py
+++ Train.py
@@ -3,51 +3,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
 import joblib
 import Analyse
 
-with h5py.File('GIB-UVA ERP-BCI.hdf5', 'r') as f:
-    for key in f.keys():
-        print(key)
-    data = f['features']
-    labels = f['erp_labels']
-    target = f['target']
-    m_d = f['matrix_dims']
-    r_i = f['run_indexes']
-    sequences = f['sequences']
-    subjects = f['subjects']
-    trials = f['trials']
-    print(target)
-    print(labels)
-    print(data)
-    print(m_d)
-    print(sequences)
-    print(r_i)
-    print(subjects)
-    print(trials)
-    l = labels[0:10000]
-    d = np.mean(data[0:10000, 0:128, :], 2)
-    t = target[4000:5000, 2]
-    sq = sequences[0:1000]
-    ri = r_i[0:1000]
-    sb = subjects[1000:2000]
-    tr = trials[701000:701615]
+def Neuronet_0(d, l, Fs):
+    data_spect = np.zeros((10000, 64))
+    N = np.shape(d)[1]
+    a = Analyse.Spectrum(d[0, :], Fs, N)
+    for i in range(0, 10000):
+        data_spect[i, :] = Analyse.Spectrum(d[i, :], Fs, N)
+    # Разделение выборки на тренировочную и тестовую
+    X_train, X_test, y_train, y_test = train_test_split(data_spect, l, test_size=0.2)
 
-data_spect = np.zeros((10000, 64))
-Fs = 128
-N = np.shape(d)[1]
-a = Analyse.Spectrum(d[0, :], Fs, N)
-for i in range(0, 10000):
-    data_spect[i, :] = Analyse.Spectrum(d[i, :], Fs, N)
-# Разделение выборки на тренировочную и тестовую
-X_train, X_test, y_train, y_test = train_test_split(data_spect, l, test_size=0.2)
+    # Создание модели многослойного перцептрона
+    model = MLPClassifier(hidden_layer_sizes=(100, 50, 25), activation='relu', solver='adam', max_iter=500)
 
-# Создание модели многослойного перцептрона
-model = MLPClassifier(hidden_layer_sizes=(100, 50, 25), activation='relu', solver='adam', max_iter=500)
+    # Обучение модели
+    model.fit(X_train, y_train)
 
-# Обучение модели
-model.fit(X_train, y_train)
+    # Сохранение модели
+    filename = 'model_params_2.pkl'
+    joblib.dump(model, filename)
 
-# Сохранение модели
-filename = 'model_params_2.pkl'
-joblib.dump(model, filename)
+def CNN(data, pred, Fs):
+    model = models.Sequential()
+
+    # 1st layer: 3 convolutional filters (64*1, 32*1, 16*1)
+    model.add(layers.Conv2D(65, (64, 1), activation='relu', input_shape=(128, 8, 1)))
+
+    # 2nd layer: 3 convolutional filters (1*8, 1*8, 1*8)
+    model.add(layers.Conv2D(8, (1, 8), activation='relu'))
+
+
+    # # 3rd layer: concatenation
+    # model.add(layers.Concatenate())
+
+    # 4th layer: average pooling (1*4)
+    model.add(layers.AveragePooling2D(pool_size=(4, 1)))
+
+    # Flatten the tensor
+    model.add(layers.Flatten())
+
+    # Add a dense layer
+    model.add(layers.Dense(2, activation='softmax'))
+    # model.summary()
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    # Train the model
+    X_train, X_test, y_train, y_test = train_test_split(data, pred, test_size=0.2)
+    model.fit(data, pred, epochs=10, validation_data=(X_test, y_test))
+
+    # Evaluate the model
+    test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
+    print("Test accuracy:", test_acc)
+    filename = 'model_params_3.pkl'
+    joblib.dump(model, filename)
